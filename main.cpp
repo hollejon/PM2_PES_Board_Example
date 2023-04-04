4d10271634b6b3fdf1249d0228b6e30421019c31
jonny$ <gaamerbit@gmail.com>
2023-04-04 23:12:12 +0200

    new test

--- main.cpp
+++ main.cpp
@@ -18,9 +18,10 @@ int main()
 {
     // states and actual state for state machine
     const int ROBOT_STATE_INIT     = 0;
-    const int ROBOT_STATE_FORWARD  = 1;
-    const int ROBOT_STATE_BACKWARD = 2;
-    const int ROBOT_STATE_SLEEP    = 3;
+    const int first_attack         = 1;
+    const int climb_wall           = 2;
+    const int align_parallel       = 3;
+    const int veni_vidi_vici       = 4;
     int robot_state_actual = ROBOT_STATE_INIT;
 
 
@@ -44,17 +45,6 @@ int main()
     mechanical_button.mode(PullUp);    // set pullup mode: sets pullup between pin and 3.3 V, so that there is a defined potential
 
 
-   
-
-
-    // Futaba Servo S3001 20mm 3kg Analog
-    Servo servo_S1(PB_2);     // create servo objects
-    Servo servo_S2(PC_8);
-    float servo_S1_angle = 0; // servo S1 normalized input: 0...1
-    float servo_S2_angle = 0;
-    
-    int servo_counter = 0;    // define servo counter, this is an additional variable to make the servos move
-    const int loops_per_seconds = static_cast<int>(ceilf( 1.0f / (0.001f * (float)main_task_period_ms) ));
 
 
     // 78:1, 100:1, ... Metal Gearmotor 20Dx44L mm 12V CB
@@ -75,20 +65,25 @@ int main()
     const float k_gear = 100.0f / 78.125f;         // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
     const float kp = 0.1f;                         // define custom kp, this is the default speed controller gain for gear box 78.125:1
 
-    SpeedController speedController_M2(counts_per_turn, kn, max_voltage, pwm_M2, encoder_M2); // default 78.125:1 gear box  with default contoller parameters
-    // SpeedController speedController_M2(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M2, encoder_M2); // parameters adjusted to 100:1 gear
+     PositionController positionController_M1(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M1, encoder_M1); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
+    positionController_M1.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
+    float max_speed_rps = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+    positionController_M1.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
+
+
+    PositionController positionController_M2(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M2, encoder_M2); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
+    positionController_M2.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
+    float max_speed_rps2 = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+    positionController_M2.setMaxVelocityRPS(max_speed_rps2); // adjust max velocity for internal speed controller
+
+
 
     // PositionController positionController_M3(counts_per_turn, kn, max_voltage, pwm_M3, encoder_M3); // default 78.125:1 gear with default contoller parameters
     PositionController positionController_M3(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M3, encoder_M3); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
     positionController_M3.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
-    float max_speed_rps = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
-    positionController_M3.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
+    float max_speed_rps3 = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+    positionController_M3.setMaxVelocityRPS(max_speed_rps3); // adjust max velocity for internal speed controller
 
-
- // Sharp GP2Y0A41SK0F, 4-40 cm IR Sensor
-    float ir_distance_mV = 0.0f; // define variable to store measurement
-    AnalogIn ir_analog_in(PC_2); // create AnalogIn object to read in infrared distance sensor, 0...3.3V are mapped to 0...1
-    float voltage_in_mv;
 
     main_task_timer.start();
     
@@ -97,108 +92,86 @@ int main()
 
         main_task_timer.reset();
 
-
-        
-
-
-
-
-
-        if (do_execute_main_task) {
-            ir_distance_mV=ir_analog_in.read();
-        voltage_in_mv = ir_distance_mV*3300;
-        printf("\n%f, ", voltage_in_mv );
-
-            // read analog input
-            ir_distance_mV = 1.0e3f * ir_analog_in.read() * 3.3f;
-
-            // visual feedback that the main task is executed, setting this once would actually be enough
+        if (do_execute_main_task)
+        {  
             additional_led = 1;
-
-            // commanding the servos
-            if (servo_S1.isEnabled() && servo_S1.isEnabled()) {
-                
-                // command servo position, increment normalised angle every second until it reaches 1.0f
-                servo_S1.setNorlalisedAngle(servo_S1_angle);
-                if (servo_S1_angle < 1.0f & servo_counter%loops_per_seconds == 0 & servo_counter != 0) {
-                    servo_S1_angle += 0.01f;
-                }
-                servo_S2.setNorlalisedAngle(servo_S2_angle);
-                if (servo_S2_angle < 1.0f & servo_counter%loops_per_seconds == 0 & servo_counter != 0) {
-                    servo_S2_angle += 0.01f;
-                }
-                servo_counter++;
-            }
+        }
+            
 
             // state machine
             switch (robot_state_actual) {
 
                 case ROBOT_STATE_INIT:
 
-                    // check if servos are enabled, should be alreay disabled at this point, it's just an example
-                    if (!servo_S1.isEnabled()) servo_S1.enable();
-                    if (!servo_S2.isEnabled()) servo_S2.enable();
+                  
 
                     enable_motors = 1; // enable hardwaredriver dc motors: 0 -> disabled, 1 -> enabled
 
-                    robot_state_actual = ROBOT_STATE_FORWARD;
+                    robot_state_actual = first_attack;
                     break;
 
-                case ROBOT_STATE_FORWARD:
 
-                    if (mechanical_button.read()) {
-                        pwm_M1.write(0.75f); // write output voltage to motor M1
-                        speedController_M2.setDesiredSpeedRPS(0.5f); // set a desired speed for speed controlled dc motors M2
-                        positionController_M3.setDesiredRotation(1.5f); // set a desired rotation for position controlled dc motors M3
-
-                        robot_state_actual = ROBOT_STATE_BACKWARD;
+                case first_attack:
+                {
+                    if(mechanical_button.read())
+                    {
+                        positionController_M1.setDesiredRotation(10.0f);
+                        positionController_M2.setDesiredRotation(10.0f);
+                        robot_state_actual= climb_wall;
+                        break;
                     }
+
+                }
+
+                case climb_wall:
+                {
+                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95)
+                    positionController_M1.setDesiredRotation(5.0f);
+                    positionController_M2.setDesiredRotation(5.0f);
+                    positionController_M3.setDesiredRotation(3.0f);
+                    robot_state_actual = align_parallel;
                     break;
 
-                case ROBOT_STATE_BACKWARD:
 
-                    if (positionController_M3.getRotation() >= 1.45f) {
-                        pwm_M1.write(0.25f);
-                        speedController_M2.setDesiredSpeedRPS(-0.5f);
-                        positionController_M3.setDesiredRotation(0.0f);
+                }
 
-                        robot_state_actual = ROBOT_STATE_SLEEP;
+                case align_parallel:
+                {
+                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95 && positionController_M3.getRotation()>=2.9)
+                    {
+                        positionController_M1.setDesiredRotation(-5.0f);
+                        positionController_M2.setDesiredRotation(-5.0f);
+                        break;
                     }
-                    break;
+                }
 
-                case ROBOT_STATE_SLEEP:
-
-                    if (positionController_M3.getRotation() <= 0.05f) {
-                        enable_motors = 0;
-                        pwm_M1.write(0.5f);
-                        speedController_M2.setDesiredSpeedRPS(0.5f);
-                        
-                        // robot_state_actual is not changed, there for the state machine remains in here until the blue button is pressed again
+                case veni_vidi_vici:
+                {
+                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95)
+                    {
+                        break;
                     }
+                }
+
+
+            
+
+
+
 
                 default:
-                
-                    // do nothing
+                {
                     break;
-            }
+            
+                }
+                    // do nothing
+                    
 
-        } else {
-
-            if (do_reset_all_once) {
+        else
+        {
+            if (do_reset_all_once) 
+            {
                 do_reset_all_once = false;
-
-                ir_distance_mV = 0.0f;
-
-                pwm_M1.write(0.5f);
-                speedController_M2.setDesiredSpeedRPS(0.0f);
-                positionController_M3.setDesiredRotation(0.0f);
-                robot_state_actual = ROBOT_STATE_INIT;
-
-                servo_S1_angle = 0;
-                servo_S2_angle = 0;
-                servo_S1.disable();
-                servo_S2.disable();
-
                 additional_led = 0;
             }            
         }
@@ -226,4 +199,11 @@ void user_button_pressed_fcn()
     // do_execute_main_task if the button was pressed
     do_execute_main_task = !do_execute_main_task;
     if (do_execute_main_task) do_reset_all_once = true;
+}
+
+
+
+
+
+
 }

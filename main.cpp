83d65855d010aa3ba363050afbfec33e9a96586b
hollejon <126668919+hollejon@users.noreply.github.com>
2024-04-08 15:32:42 +0200

    v1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <mbed.h>
 
 #include "PM2_Drivers.h"
@@ -10,18 +11,24 @@ bool do_reset_all_once = false;    // this variable is used to reset certain var
 
 // objects for user button (blue button) handling on nucleo board
 DebounceIn user_button(PC_13);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
-void user_button_pressed_fcn(); // custom functions which get executed when user button gets pressed, definition below
+void user_button_pressed_fcn();
+
+ // custom functions which get executed when user button gets pressed, definition below
 
 
+float angle_in_degree(float angle)
+{
+float value;
+value = (1.0f/360.0f)*angle;
+return value;
+}
 // main runs as an own thread
 int main()
 {
     // states and actual state for state machine
     const int ROBOT_STATE_INIT     = 0;
-    const int first_attack         = 1;
-    const int climb_wall           = 2;
-    const int align_parallel       = 3;
-    const int veni_vidi_vici       = 4;
+    const int compress_bag         = 1;
+    const int release_bag          = 2;
     int robot_state_actual = ROBOT_STATE_INIT;
 
 
@@ -29,7 +36,7 @@ int main()
     user_button.fall(&user_button_pressed_fcn);
 
     // while loop gets executed every main_task_period_ms milliseconds (simple aproach to repeatedly execute main)
-    const int main_task_period_ms = 1000; // define main task period time in ms e.g. 50 ms -> main task runs 20 times per second
+    const int main_task_period_ms = 10; // define main task period time in ms e.g. 50 ms -> main task runs 20 times per second
     Timer main_task_timer;              // create Timer object which we use to run the main task every main_task_period_ms
 
 
@@ -50,6 +57,7 @@ int main()
     // 78:1, 100:1, ... Metal Gearmotor 20Dx44L mm 12V CB
     DigitalOut enable_motors(PB_15); // create DigitalOut object to enable dc motors
 
+
     FastPWM pwm_M1(PB_13); // motor M1 is used open-loop
     FastPWM pwm_M2(PA_9);  // motor M2 is closed-loop speed controlled (angle velocity)
     FastPWM pwm_M3(PA_10); // motor M3 is closed-loop position controlled (angle controlled)
@@ -58,124 +66,169 @@ int main()
     EncoderCounter  encoder_M2(PB_6, PB_7);
     EncoderCounter  encoder_M3(PA_0, PA_1);
 
-    // create SpeedController and PositionController objects, default parametrization is for 78.125:1 gear box
-    const float max_voltage = 12.0f;               // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
+
+/*
+   const float max_voltage = 12.0f;               // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
     const float counts_per_turn = 20.0f * 78.125f; // define counts per turn at gearbox end: counts/turn * gearratio
     const float kn = 180.0f / 12.0f;               // define motor constant in RPM/V
     const float k_gear = 100.0f / 78.125f;         // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
-    const float kp = 0.1f;                         // define custom kp, this is the default speed controller gain for gear box 78.125:1
+    const float kp = 0.2f;                         // define custom kp, this is the default speed controller gain for gear box 78.125:1
+    */
+    
+    /*//const float counts_per_turn_M1 = 20.0f*250.0f;
+    const float counts_per_turn = 20.0f * 78.125f;
+    const float max_voltage = 12.0f;  
+    const float k_gear_M1= 250.0f/78.125f;
+    const float knM1 = 55.0f/12.0f;     
+    const float kp_M1 = 0.1;
+       
+    */
+    const float counts_per_turn = 20.0f * 488.0f;
+    const float max_voltage = 12.0f;  
+    const float k_gear_M1= 78.0f/78.125f;
+    const float knM1 = 55.0f/12.0f;     
+    const float kp_M1 = 0.1;
+    
+    PositionController positionController_M1(counts_per_turn *k_gear_M1, knM1 / k_gear_M1, max_voltage, pwm_M1, encoder_M1); 
+    // parameters adjusted to 100:1 gear, we need a different speed controller gain here
+    positionController_M1.setSpeedCntrlGain(kp_M1 * k_gear_M1);   // adjust internal speed controller gain, this is just an example
 
-     PositionController positionController_M1(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M1, encoder_M1); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
-    positionController_M1.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
-    float max_speed_rps = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+    
+    float max_speed_rps = 0.9f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+    /*
     positionController_M1.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
-
-
-    PositionController positionController_M2(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M2, encoder_M2); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
-    positionController_M2.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
-    float max_speed_rps2 = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
-    positionController_M2.setMaxVelocityRPS(max_speed_rps2); // adjust max velocity for internal speed controller
-
-
-
-    // PositionController positionController_M3(counts_per_turn, kn, max_voltage, pwm_M3, encoder_M3); // default 78.125:1 gear with default contoller parameters
-    PositionController positionController_M3(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M3, encoder_M3); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
-    positionController_M3.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
-    float max_speed_rps3 = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
-    positionController_M3.setMaxVelocityRPS(max_speed_rps3); // adjust max velocity for internal speed controller
-
-
+    */
     main_task_timer.start();
+
+
+    float offset_M1=0;
+    
+    
     
     // this loop will run forever
     while (true) {
+            
+        
+        
+      
 
+      
+         // printf("Angle: %f\n", positionController_M1.getRotation()*360.0f);
+            
+        
+        
+    
+                
         main_task_timer.reset();
+
+
+        
 
         if (do_execute_main_task)
         {  
+
+
+        
             additional_led = 1;
-        }
-            
+        
+        
 
             // state machine
             switch (robot_state_actual) {
 
+
                 case ROBOT_STATE_INIT:
-
-                  
-
-                    enable_motors = 1; // enable hardwaredriver dc motors: 0 -> disabled, 1 -> enabled
-
-                    robot_state_actual = first_attack;
-                    break;
-
-
-                case first_attack:
                 {
                     if(mechanical_button.read())
                     {
-                        positionController_M1.setDesiredRotation(10.0f);
-                        positionController_M2.setDesiredRotation(10.0f);
-                        robot_state_actual= climb_wall;
-                        break;
+                    enable_motors=1;
+                    
+                    robot_state_actual=compress_bag;
                     }
-
-                }
-
-                case climb_wall:
-                {
-                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95)
-                    positionController_M1.setDesiredRotation(5.0f);
-                    positionController_M2.setDesiredRotation(5.0f);
-                    positionController_M3.setDesiredRotation(3.0f);
-                    robot_state_actual = align_parallel;
                     break;
-
-
                 }
 
-                case align_parallel:
+
+                case compress_bag:
                 {
-                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95 && positionController_M3.getRotation()>=2.9)
+
+                    
+  
+                     //const float counts_per_turn_M1 = 20.0f*250.0f;
+   
+    
+    
+                     max_speed_rps = 0.25f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+                    positionController_M1.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
+
+
+                    positionController_M1.setDesiredRotation(offset_M1+angle_in_degree(-115.0f));
+
+                    printf("Angle: %f\n", positionController_M1.getRotation()*360.0f);
+                  
+                        if(positionController_M1.getRotation() <= offset_M1+angle_in_degree(-110.0f))
                     {
-                        positionController_M1.setDesiredRotation(-5.0f);
-                        positionController_M2.setDesiredRotation(-5.0f);
-                        break;
+                        
+
+                        
+    
+    
+                      
+                        robot_state_actual=release_bag;
+                       
+
+                        offset_M1=positionController_M1.getRotation();
+                       
                     }
+
+                   
+                    break;
                 }
 
-                case veni_vidi_vici:
+
+
+
+
+                case release_bag:
                 {
-                    if (positionController_M2.getRotation() >= 9.95f && positionController_M1.getRotation()>=9.95)
-                    {
+
+                        max_speed_rps = 0.12f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
+                        positionController_M1.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
+   
+                          
+                        positionController_M1.setDesiredRotation(offset_M1+angle_in_degree(115.0f));
+
+                        if(positionController_M1.getRotation() >=offset_M1+ angle_in_degree(110.0f))
+                        {
+                            
+                            robot_state_actual=compress_bag;
+                            offset_M1=positionController_M1.getRotation();
+                           
+                            
+                        }
                         break;
-                    }
+                    
                 }
 
 
-            
-
-
-
+                
+                
+                
 
                 default:
                 {
-                    break;
-            
+                    enable_motors=0;
                 }
-                    // do nothing
                     
+                          
+                           
+                         
+                        
+                   
+        }      
+              
 
-        else
-        {
-            if (do_reset_all_once) 
-            {
-                do_reset_all_once = false;
-                additional_led = 0;
-            }            
-        }
-
+       
         // toggling the user led
         user_led = !user_led;
 
@@ -191,7 +244,7 @@ int main()
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
         thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
-    }
+    }}
 }
 
 void user_button_pressed_fcn()
@@ -199,11 +252,13 @@ void user_button_pressed_fcn()
     // do_execute_main_task if the button was pressed
     do_execute_main_task = !do_execute_main_task;
     if (do_execute_main_task) do_reset_all_once = true;
-}
-
-
-
-
-
 
 }
+
+
+
+
+
+
+
+
